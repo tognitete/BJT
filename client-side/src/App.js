95f1d6732dc667c03ac86bf10a8c77bd73724fd3
Tetevi Fiacre TOGNI <tetevi-fiacre.togni@etu.unice.fr>
2020-02-13 17:57:47 +0100

    #22:navbar adding

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,8 +1,9 @@
-
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Description from './Components/description';
+import Navbar from './Components/Navbar/Navbar' ;
+import { BrowserRouter , Switch , Route } from 'react-router-dom';
 
 //import SimpleForm from './Components/form';
 import ReactDOM from 'react-dom';
@@ -11,9 +12,25 @@ import Form from './Components/formulaire/formulaire';
 
 function App() {
   return (
-    <div className="App">
-       <Form />
-    </div>
+    <>
+     <BrowserRouter>
+
+       <Navbar/>
+
+       <div className="App">
+       <Switch>
+           <Route path="/description">
+            <Description />
+          </Route>
+          <Route path="/">
+            <Form />
+          </Route>
+        </Switch>
+      </div>
+
+     </BrowserRouter>
+   
+   </>
   );
 }
 export default App;
