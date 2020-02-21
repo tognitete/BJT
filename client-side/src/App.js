2dd8bb114eba6f549c427e2056dee9072afca348
Manel Benhissi <manel.benhissi@etu.unice.fr>
2020-02-21 17:12:56 +0100

    merge fix

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,28 +1,5 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Description from './Components/description';
-//import LoginForm from './Components/login/login';
-
-
-//import SimpleForm from './Components/form';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-//import {BrowserRouter, Switch, Route} from "react-router-dom"
-import Description from './Components/PluginsETdescriptions/description';
-import Navbar from './Components/Navbar/Navbar' ;
-import { BrowserRouter , Switch , Route } from 'react-router-dom';
-
-//import SimpleForm from './Components/form';
-//import ReactDOM from 'react-dom';
-//import SimpleForm from './Components/form';
-import Form from './Components/formulaire/formulaire';
-import Login from './Components/login/login';
-import TesterPlugin from './Components/PluginsETdescriptions/testerPlugin'
-import Signup from './Components/login/signup'
-
-
-=======
 import Description from './Components/PluginsETdescriptions/description';
 import Navbar from './Components/Navbar/Navbar' ;
 import { BrowserRouter , Switch , Route } from 'react-router-dom';
@@ -30,7 +7,6 @@ import Form from './Components/formulaire/formulaire';
 import Login from './Components/login/login';
 import Signup from './Components/login/signup';
 import Logout from './Components/login/logout'
->>>>>>> 8fa6a5b0bfc427ceaa1df8507bb27358bcbe0146
 import AffichagePlugins from './Components/PluginsETdescriptions/AffichagePlugins'
 
 
@@ -71,10 +47,6 @@ function App(props) {
      </BrowserRouter>
    
    </>
-<<<<<<< HEAD
-=======
-
->>>>>>> 8fa6a5b0bfc427ceaa1df8507bb27358bcbe0146
   );
   
   
